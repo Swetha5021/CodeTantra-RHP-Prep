2dc08afc4cd64e7e2b758958280bbd9dcd3ac4a3
Swetha S <swethasivasankararaj.05@gmail.com>
2024-04-03 23:10:27 +0530

    Add Day1-GeometricSum.java and Day1-RoverWalk.java

---
+++ Day1-GeometricSum.java
@@ -0,0 +1,42 @@
+/*
+Problem Statement:
+Your are given a positive integer k.
+Your task is to find the geometric sum i.e. 1 + 1/2 + 1/4 + 1/8 + _______ + 1/(2^k)
+Note: Use recursion to implement your solution.
+Your task is to complete function geometricSum with following parameters passed:
+integer k: k is a positive integer defines the limit up to which you have to find the geometric sum.
+Function returns:
+float : Return the geometric sum i.e. as real number.
+
+Constraints to be followed:
+0 <= k <= 100
+Sample test case 1:
+Input:
+3 // value of k
+Output:
+1.875000 // geometric sum up to 5 decimal places
+Sample test case 2:
+Input:
+4
+Output:
+1.937500
+Brief editorial: in test case 1 geometric sum will be calculated as: 1+ 1/(2^1) + 1/(2^2) + 1/(2^3) = 1.875
+*/ 
+
+import java.util.*;
+public class Main
+{
+    public static double findGeometricSum(int k){
+		if(k==0)
+		{
+			return 1;
+		}
+		return (1/(Math.pow(2, k)))+findGeometricSum(k-1); 
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int k=sc.nextInt();
+		System.out.printf("%.5f",findGeometricSum(k));
+	}
+}
---
+++ Day1-RoverWalk.java
@@ -0,0 +1,52 @@
+/*
+Problem Statement:
+A Rover sent to moon wants to move in a research area land in form grid of size M x N. (M- Rows N- Columns). Rover starts from (0,0) coordinates and it has to reach to destination (M-1,N-1) coordinates. 
+Note: Rover can only move right or down. 
+You have to compute the total number of ways rover can reach the destination.
+Complete the function RoverWalk with parameters:
+integer M,N : It specifies the the Number of rows and columns in the area grid.
+
+Function returns:
+integer: the count of number of possible ways for rover to reach (0,0) to (M-1,N-1) destination coordinates.
+Note: Both M and N may be equal or not (i.e grid can be square as well as rectangular in shape
+
+Constraints to be followed:
+1 <= M,N <=500
+Sample testcase 1:
+Input:
+2 2
+Output:
+2
+Sample testcase 2:
+Input:
+5 5
+Output:
+70
+*/
+
+import java.util.*;
+public class Main {
+    public static int countWays(int M, int N) {
+        int[][] dp = new int[M][N];
+        
+        for (int i = 0; i < M; i++)
+            dp[i][0] = 1;
+        for (int j = 0; j < N; j++)
+            dp[0][j] = 1;
+        
+        for (int i = 1; i < M; i++) {
+            for (int j = 1; j < N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[M - 1][N - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        System.out.println(countWays(M, N));
+    }
+}
+
