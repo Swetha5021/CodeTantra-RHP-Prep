ad7e5a5904261c6b82b08126350de0908d0b8661
Swetha S <swethasivasankararaj.05@gmail.com>
2024-07-10 20:50:33 +0530

    added a program

---
+++ Day33-NoOfWays.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Main {
+
+    public static int noOfWays(int a[][], int n, int i, HashSet < Integer > set) {
+        // Base
+        if (i == n)
+            return 1;
+        int counter = 0;
+        for (int j = 0; j < n; j++) {
+
+            if (a[i][j] == 1 && !set.contains(j)) {
+                set.add(j);
+                counter = counter + noOfWays(a, n, i + 1, set);
+                // bactracking
+                set.remove(j);
+            }
+
+        }
+        return counter;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // no nodes / vertices
+        int a[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        HashSet < Integer > set = new HashSet < > ();
+        System.out.print(noOfWays(a, n, 0, set));
+        sc.close();
+
+    }
+}
