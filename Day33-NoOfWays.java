7b031aaee25e5de8604b7b3119f8dcd249f8b56b
Swetha S <swethasivasankararaj.05@gmail.com>
2024-07-11 21:10:20 +0530

    updated

--- Day33-NoOfWays.java
+++ Day33-NoOfWays.java
@@ -100,50 +100,58 @@ class Main {
 }
 
 //other approach
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
+class Main {
 
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int[][] mat = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = s.nextInt();
-            }
-        }
-        System.out.print(numberOfWays(mat, n));
-        s.close();
-    }
-
-    static int numberOfWays(int[][] mat, int n) { // O(n*n*(2^n))
-        //every entry in table[i][j] in this table represents
+    public static int noOfWays(int a[][], int n) {
+        // every entry in table[i][j] this table represents
         // count of ways to make pairs between i red balls and n blue balls
-        int[][] table = new int[n + 1][1 << n];
-        //there is no red ball and no blue ball, count of ways to make n pair is 1
-        table[0][0] = 1;
+        int table[][] = new int[n + 1][1 << n];
 
-        //outer loop i represents red balls
+        table[0][0] = 1; // their is no red and no blue ball, count of ways to make n pair is 1
+        // outer loop i represents red balls
         for (int i = 0; i < n; i++) {
-            // j ideally generates a permutation of blue ball 0 -> selected 1 -> selected
+            // j ideally generates a permutation of blue ball 0 selected 1 not selected
             for (int j = 0; j < 1 << n; j++) {
+
                 // iterate on the range 0 to n
                 for (int k = 0; k < n; k++) {
-                    //Creating a mask with the kth bit as set bit
-                    int mask = 1 << k;
-                    // if the kth bit of the mask is not set and mat[i][k] = 1
-                    if ((mask & j) == 0 && mat[i][k] == 1) { // 010 | 000 = 010
+                    // Creating a mask with the k bit as a set bit
+                    int mask = 1 << k; // 10
+                    // if the kth bit of the mask is not set and a[i][k] is 1
+                    if ((mask & j) == 0 && a[i][k] == 1) { // 010 | 000 = 010
                         table[i + 1][j | mask] = table[i + 1][j | mask] + table[i][j];
+                        System.out.print(table[i + 1][j | mask] + " ");
+
                     }
                 }
-            }
-        }
 
+            }
+
+        }
+        System.out.println();
         for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < (1 << n); j++) {
+            for (int j = 0; j < 1 << n; j++) {
                 System.out.print(table[i][j] + " ");
             }
             System.out.println();
         }
+
         return table[n][(1 << n) - 1];
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // no nodes / vertices
+        int a[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        //HashSet<Integer> set = new HashSet<>();
+        System.out.print(noOfWays(a, n));
+        sc.close();
+
     }
 }
